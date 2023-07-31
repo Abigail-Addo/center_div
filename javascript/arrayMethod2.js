643aee5230075ed6889462a204163a2b2626b74c
Abigail <addoa350@gmail.com>
2023-07-31 12:04:01 +0000

    Array methods

---
+++ javascript/arrayMethod2.js
@@ -0,0 +1,12 @@
+// const temperature = [
+//     33, 12, 20, 16, 5, 64, 32,
+//     {name: "temp",  patientId: 4, clinic :"Nyaho"},
+//     {name: "temp",  patientId: 5, clinic :"Nyaho"},
+// ];
+// console.log(temperature[2]);
+
+
+const student = {firstName: "Abigil", lastName: "Addo", idNum: 1, course: "Engineering", email: "addoa350@gmail.com", age: 32,};
+const studentArray = Object.keys(student);
+studentArray.sort();
+console.log(studentArray);
