e6f95a6d00b267f75c40fd87358c822200e9da2e
Abi <135342726+Abigail-Addo@users.noreply.github.com>
2023-08-03 10:31:34 +0000

    array methods

---
+++ test.js
@@ -0,0 +1,7 @@
+const student = {firstName: "Abigail", lastName: "Addo", idNum: 1, course: "Economics", email: "addoa350@gmail.com", age: 32,
+  
+};
+
+const studentArray = Object.keys(student);
+studentArray.sort();
+console.log(studentArray);
